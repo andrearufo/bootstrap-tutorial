aaef399388a054db6a01021aa972858607491d86
andrearufo <info@andrearufo.it>
2020-10-28 06:43:33 +0100

    Aggiustamenti e migliorie

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 
 function build(cb) {
     return src('dev/styles.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write())
         .pipe(dest('.'));
 
         cb();
